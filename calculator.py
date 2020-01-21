ce933c66b2a3c7a8681eba499f096ad82e077225
Hansol Chang <hc270@duke.edu>
2020-01-21 16:17:33 -0500

    Started calculator module

--- calculator.py
+++ calculator.py
@@ -1,20 +1,21 @@
 # calculator.py
 
-def add(x, y):
-    z = x + y
-    print("{} + {} = {}".format(x,y,z))
-    return z
-    
+def add (x, y) :
+	z = x + y
+	print("{} + {} = {}".format(x,y,z))
+	return z
+
 x = input("Enter a letter: ")
 print("You entered {}".format(x))
 if x == "a":
-d= add(56, 73)
+	d = add(56,73)
+	if d > 100:
+		print("This number is too high.")
 elif x == "s":
-a=20
-b = -3
-c = a - b
-print("{} - {} = {}".format(a,b,c))
+	a = 20
+	b = -3
+	c = a - b
+	print("{} - {} = {}".format(a,b,c))
 else:
-	print("The {} command is not recognized.".format(a,b,c))
-
+	print("The {} command is not recognized.".format(x))
 print("DONE")
