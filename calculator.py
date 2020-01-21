26651fd409ac49d92115ae29fa40048f2e7a1405
Hansol Chang <hc270@duke.edu>
2020-01-21 16:30:29 -0500

    enter numbers and operation

--- calculator.py
+++ calculator.py
@@ -5,17 +5,35 @@ def add (x, y) :
 	print("{} + {} = {}".format(x,y,z))
 	return z
 
-x = input("Enter a letter: ")
-print("You entered {}".format(x))
-if x == "a":
-	d = add(56,73)
-	if d > 100:
-		print("This number is too high.")
-elif x == "s":
-	a = 20
-	b = -3
-	c = a - b
-	print("{} - {} = {}".format(a,b,c))
+def subtract (x,y) :
+	z = x - y
+	print("{} - {} = {}".format(x,y,z))
+	return z
+
+def multiply (x,y) :
+	z = x * y
+	print("{} * {} = {}".format(x,y,z))
+	return z
+
+def divide (x,y) :
+	z = x / y
+	print("{} / {} = {}".format(x,y,z))
+	return z
+
+x = input("Enter a first number: ")
+y = input("Enter a second number: ")
+z = input("Enter a operation: ")
+a = float(x)
+b = float(y)
+print("Two numbers are {} and {}. Operation is {}".format(x,y,z))
+if z == "add":
+	add(a, b)
+elif z == "subtract":
+	subtract(a,b)
+elif z == "multiply":
+	multiply(a,b)
+elif z == "divide":
+	divide(a,b)
 else:
-	print("The {} command is not recognized.".format(x))
+	print("The {} operation is not recognized.".format(z))
 print("DONE")
